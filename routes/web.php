8e69fda291e764bfc645bbe7f77fdaeef1026270
Made Dewantara <76637189+madewantara@users.noreply.github.com>
2022-03-21 23:40:11 +0700

    feat : add routing account and project

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FindivDashboardController;
+use App\Http\Controllers\FindivAccountController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +19,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
+
+Route::group([
+    'middleware' => ['finance.division'],
+    'prefix' => 'finance-division'
+], function ($router) {
+    Route::get('/dashboard', [App\Http\Controllers\FindivDashboardController::class, 'index'])->name('findiv.dashboard');
+
+    //Account Menu
+    Route::get('/account', [App\Http\Controllers\FindivAccountController::class, 'index'])->name('findiv.account-index');
+    Route::get('/account/create', [App\Http\Controllers\FindivAccountController::class, 'create'])->name('findiv.account-create');
+    Route::post('/account/post', [App\Http\Controllers\FindivAccountController::class, 'store'])->name('findiv.account-post');
+    Route::get('/account/edit/{uuid}', [App\Http\Controllers\FindivAccountController::class, 'edit'])->name('findiv.account-edit');
+    Route::post('/account/update/{uuid}', [App\Http\Controllers\FindivAccountController::class, 'update'])->name('findiv.account-update');
+    Route::delete('/account/update/{uuid}', [App\Http\Controllers\FindivAccountController::class, 'destroy'])->name('findiv.account-destroy');
+    Route::get('/account/export', [App\Http\Controllers\FindivAccountController::class, 'export'])->name('findiv.account-export');
+
+    //Project Menu
+    Route::get('/project', [App\Http\Controllers\FindivProjectController::class, 'index'])->name('findiv.project-index');
+    Route::get('/project/{uuid}', [App\Http\Controllers\FindivProjectController::class, 'show'])->name('findiv.project-detail');
+
+    //Profile
+    Route::get('/profile', [App\Http\Controllers\FindivProfileController::class, 'index'])->name('findiv.profile');
+});
 
 require __DIR__.'/auth.php';
