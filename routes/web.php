586cf949ece13a08fd581d5419e1ea196ec811a8
Made Dewantara <76637189+madewantara@users.noreply.github.com>
2022-04-01 02:08:41 +0700

    refactor : route findiv

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,22 @@ Route::group([
     Route::post('/account/post', [App\Http\Controllers\FindivAccountController::class, 'store'])->name('findiv.account-post');
     Route::get('/account/edit/{uuid}', [App\Http\Controllers\FindivAccountController::class, 'edit'])->name('findiv.account-edit');
     Route::post('/account/update/{uuid}', [App\Http\Controllers\FindivAccountController::class, 'update'])->name('findiv.account-update');
-    Route::delete('/account/update/{uuid}', [App\Http\Controllers\FindivAccountController::class, 'destroy'])->name('findiv.account-destroy');
+    Route::delete('/account/delete/{uuid}', [App\Http\Controllers\FindivAccountController::class, 'destroy'])->name('findiv.account-destroy');
     Route::get('/account/export', [App\Http\Controllers\FindivAccountController::class, 'export'])->name('findiv.account-export');
+    
+    //Cash Menu
+    Route::get('/cash', [App\Http\Controllers\FindivCashController::class, 'index'])->name('findiv.cash-index');
+    Route::get('/cash/create', [App\Http\Controllers\FindivCashController::class, 'create'])->name('findiv.cash-create');
+    Route::post('/cash/post', [App\Http\Controllers\FindivCashController::class, 'store'])->name('findiv.cash-post');
+    Route::get('/cash/{uuid}', [App\Http\Controllers\FindivCashController::class, 'show'])->name('findiv.cash-detail');
+    Route::get('/cash/edit/{uuid}', [App\Http\Controllers\FindivCashController::class, 'edit'])->name('findiv.cash-edit');
+    Route::delete('/cash/delete/{uuid}', [App\Http\Controllers\FindivCashController::class, 'destroy'])->name('findiv.cash-destroy');
+    Route::post('/cash/export', [App\Http\Controllers\FindivCashController::class, 'export'])->name('findiv.cash-export');
+
+    //Mandiri Operational Menu
+
+    //Mandiri Escrow Menu
+
 
     //Project Menu
     Route::get('/project', [App\Http\Controllers\FindivProjectController::class, 'index'])->name('findiv.project-index');
