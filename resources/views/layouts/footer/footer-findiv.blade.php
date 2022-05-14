1434baf38e83b8eb0951a094a7b3d33ddb4292fa
Made Dewantara <76637189+madewantara@users.noreply.github.com>
2022-05-14 23:31:23 +0700

    refactor : change footer

--- resources/views/layouts/footer/footer-findiv.blade.php
+++ resources/views/layouts/footer/footer-findiv.blade.php
@@ -4,8 +4,10 @@
     <div class="container d-flex flex-column flex-md-row flex-stack">
         <!--begin::Copyright-->
         <div class="text-dark order-2 order-md-1">
-            <span class="text-gray-400 fw-bold me-1">Created by</span>
-            <a href="https://www.linkedin.com/in/madedewantara" target="_blank" class="text-muted text-hover-primary fw-bold me-2 fs-6">Made Dewantara</a>
+            <span class="text-gray-400 fw-bold me-1">Copyright&copy; {{ now()->year }}</span>
+            <a href="https://eecindonesia.co.id/" target="_blank" class="text-muted text-hover-primary fw-bold fs-6">EEC
+                Indonesia</a>
+            <span class="text-gray-400 fw-bold me-1">All rights reserved.</span>
         </div>
         <!--end::Copyright-->
     </div>
