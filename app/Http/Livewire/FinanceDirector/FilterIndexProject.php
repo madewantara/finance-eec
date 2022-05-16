b250c2bde1320cad9497c431d02dfceb6951d8fa
Made Dewantara <76637189+madewantara@users.noreply.github.com>
2022-05-16 23:44:16 +0700

    refactor : change order by to last updated project

--- app/Http/Livewire/FinanceDirector/FilterIndexProject.php
+++ app/Http/Livewire/FinanceDirector/FilterIndexProject.php
@@ -23,8 +23,10 @@ class FilterIndexProject extends Component
         $projHigh = Project::where([['is_active',1],['status', 1],['priority', 1]])->get();
         $projMed = Project::where([['is_active',1],['status', 1],['priority', 2]])->get();
         $projLow = Project::where([['is_active',1],['status', 1],['priority', 3]])->get();
-        $projCategory = Project::where([['is_active',1], ['status', 'like', '%'.$this->status.'%'], ['name', 'like', '%'.$this->search.'%']])->with("projectCategory")->orderBy('status', 'asc')->paginate($this->pagesize);
+        $projCategory = Project::where([['is_active',1], ['status', 'like', '%'.$this->status.'%'], ['name', 'like', '%'.$this->search.'%']])->with("projectCategory")->orderBy('created_at', 'desc')->paginate($this->pagesize);
         $arrhighProjectExpanse = $this->arrhighProjectExpanse;
+
+        $this->emit('refreshDropdown');
         
         return view('livewire.finance-director.filter-index-project', ['projCategory' => $projCategory, 'allProj' => $allProj, 'projHigh' => $projHigh, 'projMed' => $projMed, 'projLow' => $projLow, 'arrhighProjectExpanse' => $arrhighProjectExpanse]);
     }
